cab71edfafddf2d79c598daa8d7837745b229cf6
Ryan Wiener <ryanl.wiener@yahoo.com>
2019-11-11 19:33:29 -0800

    Got login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root 'posts#index'
+
   resources :messages
   resources :comments
   resources :posts
   resources :users
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
