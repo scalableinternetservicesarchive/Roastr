3038c37c328e9182d5e2e9844d7cec676094f8f8
Jake Guida <jlguida@ucsb.edu>
2019-12-04 22:43:45 -0800

    paginate with SQL optimizations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.left_outer_joins(:user).select('posts.*, users.username').order("posts.created_at DESC")
-    #@posts = Post.all.order("created_at DESC")
-    ## perform a paginated query:
-    @posts = Post.paginate(page: params[:page], per_page: 1).order("created_at DESC")
-
+    @posts = Post.left_outer_joins(:user).select('posts.*, users.username').paginate(page: params[:page], per_page: 2).order("posts.created_at DESC")
   end
 
   # GET /posts/1
