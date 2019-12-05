c2cda31794e2c50041c11e1b22f052d71647173a
Jake Guida <jlguida@ucsb.edu>
2019-12-05 14:04:34 -0800

    finished styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.paginate(page: params[:page], per_page: 25)
   end
 
   # GET /users/1
